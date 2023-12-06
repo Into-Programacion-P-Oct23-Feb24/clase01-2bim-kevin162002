bae96022fa30a470e1ddd7412ef146d3d239a2bf
Kevin Vicente <kcvicente@utpl.edu.ec>
2023-12-06 16:42:00 -0500

    Cambios, hacer revisar la otra semana

--- ManejoCicloFor/src/paquete03/Ejemplo03.java
+++ ManejoCicloFor/src/paquete03/Ejemplo03.java
@@ -4,7 +4,8 @@
  * and open the template in the editor.
  */
 package paquete03;
-
+import java.util.Scanner;
+import java.util.Locale;
 /**
  *
  * @author reroes
@@ -13,8 +14,34 @@ public class Ejemplo03 {
     
     public static void main(String[] args) {
         
-        for (int i = 0; i < 10; i++) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        int limite = entrada.nextInt();
+        String nombre;
+        String posicion;
+        int edad;
+        double estatura;
+        String mensajeFinal = "";
+        
+        
+        for (int i = 1; i < 10; i++) {
+            System.out.println("Ingrese el nombre del jugador");
+            nombre = entrada.nextLine();
+            System.out.println("Ingrese la posicion del jugador");
+            posicion = entrada.nextLine();
+            System.out.println("Ingrese la edad del jugador");
+            edad = entrada.nextInt();
+            System.out.println("Ingrese la estatura del jugador");
+            estatura = entrada.nextDouble();
             
+            mensajeFinal = String.format("%s%s%s%s edad%s estatura %.2f"
+                    ,mensajeFinal,
+                    i,
+                    nombre,
+                    posicion,
+                    edad,
+                    estatura);
         }
+        System.out.printf("%s\n", mensajeFinal);
     }
 }
